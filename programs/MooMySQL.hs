35c39c16f9770fc3fbc11a2953c722b02d1c68db
Bastian Krol <bastian.krol@web.de>
2016-10-18 15:51:50 +0200

    separate types for configuration and executable parameters

--- programs/MooMySQL.hs
+++ programs/MooMySQL.hs
@@ -13,21 +13,16 @@ import Moo.Main
 
 main :: IO ()
 main = do
-  -- make sure the configuration validation does not complain about a missing
-  -- db type
-  setEnv "DBM_DATABASE_TYPE" "mysql"
-
   args <- getArgs
   (_, opts, _) <- procArgs args
   conf <-
     loadConfiguration $ _configFilePath opts
   case conf of
-      Left e -> putStrLn e >> exitFailure
-      Right preliminaryConf -> do
-        let Left preliminaryDatabaseConf =  _backendOrConf preliminaryConf
-            connectionString = _connectionString preliminaryDatabaseConf
-        connection <- connectMySQL connectionString
-        let backend = mysqlBackend connection
-            finalConf = preliminaryConf { _backendOrConf = Right backend }
-        mainWithConf args finalConf
+    Left e -> putStrLn e >> exitFailure
+    Right conf -> do
+      let connectionString = _connectionString conf
+      connection <- connectMySQL connectionString
+      let backend = mysqlBackend connection
+          parameters = makeParameters conf backend
+      mainWithParameters args parameters
 
